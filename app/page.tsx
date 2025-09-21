c9dcebce69a79659db974435ebf94fafb042c78a
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2025-09-21 19:03:23 +0530

    add main components for the dessert landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Map from "./components/Map";
+import Story from "./components/Story";
+
 export default function Home() {
-  return <h1>Dave's Desserts</h1>;
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Gallery />
+      <Story />
+      <Contact />
+      <Map />
+    </main>
+  );
 }
